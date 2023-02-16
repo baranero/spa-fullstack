b30a0c3b77bbc45525c077a73039fb315b50c12a
baranero <jakub.baran2@gmail.com>
2023-02-16 08:56:09 +0100

    returning res in loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,7 +3,8 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
 
-  const events = useLoaderData()
+  const data = useLoaderData()
+  const events = data.events
 
   return (
     <>
@@ -20,7 +21,6 @@ export async function loader() {
     if (!response.ok) {
       // ...
     } else {
-      const resData = await response.json();
-      return resData.events
+      return response
     }
   }
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,11 +1,19 @@
 import { Fragment } from "react"
-import { Outlet } from "react-router"
+import { Outlet, useNavigation } from "react-router"
 import MainNavigation from "../components/MainNavigation"
 
 function RootLayout() {
+
+  // const navigation = useNavigation()
+
+
+
   return <Fragment>
     <MainNavigation/>
-    <main><Outlet/></main>
+    <main>
+      {/* { navigation.state === 'loading' && <p>Loading...</p> } */}
+      <Outlet/>
+    </main>
   </Fragment>
 }
 
