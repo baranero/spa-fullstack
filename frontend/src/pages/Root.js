f2db572552ce4999e55ecee25d80d9cd454cbb21
baranero <jakub.baran2@gmail.com>
2023-02-20 12:05:57 +0100

    added auto logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,22 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useNavigation, useRouteLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
+  const token = useLoaderData()
+  const submit = useSubmit()
+  
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return
+    }
+
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'})
+    }, 1 * 60 * 60 * 1000)
+  }, [token, submit])
 
   return (
     <>
